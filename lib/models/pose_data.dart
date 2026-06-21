c1ca143d3b6a1a6b6336ac58d4e3e2f429b43102
Yang20040522 <411630956@o365.tku.edu.tw>
2026-06-21 18:36:24 +0800

    清空 camera 分支內容

--- lib/models/pose_data.dart
+++
@@ -1,9 +0,0 @@
-// lib/models/pose_data.dart
-import 'dart:ui';
-
-class PoseData {
-  final List<Offset> keypoints;
-  final List<double> scores;
-  const PoseData(this.keypoints, this.scores);
-  static PoseData empty() => const PoseData([], []);
-}
