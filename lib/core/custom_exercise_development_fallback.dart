936d3fcd39de077fc084e38780a3a019be31efa8
chenyokuo <kuojack703@gmail.com>
2026-09-04 16:46:25 +0800

    更新治療師授權/資安措施

--- lib/core/custom_exercise_development_fallback.dart
+++ lib/core/custom_exercise_development_fallback.dart
@@ -5,7 +5,7 @@ class CustomExerciseDevelopmentFallback {
 
   // TODO(M6B deployment):
   // Remove temporary no-token development fallback after the updated backend is deployed.
-  static const bool allowMissingToken = true;
+  static const bool allowMissingToken = false;
 
   static const String remoteUnavailableMessage =
       '開發模式：新版伺服器尚未部署，雲端已儲存自訂動作暫時無法使用。';
